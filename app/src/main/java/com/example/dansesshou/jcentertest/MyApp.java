cdcbbeb64e52f96a7629f48949689a6470f4a61f
dxsdyhm <duanxuesong12@126.com>
2017-03-06 17:07:22 +0800

    1.补充版本号参数说明

--- app/src/main/java/com/example/dansesshou/jcentertest/MyApp.java
+++ app/src/main/java/com/example/dansesshou/jcentertest/MyApp.java
@@ -14,6 +14,7 @@ public class MyApp extends Application {
     //two parems come frome Gwell , the value below just test
     public final static String APPID="1e9a2c3ead108413e8218a639c540e44";
     public final static String APPToken="7db7b2bff80a025a3dad546a4d5a6c3ee545568d4e0ce9609c0585c71c287d08";
+    //前两位是客户APP唯一编号(00.00 由技威分配),后两位是APP版本号(客户自定义),此参数不可省略
     public final static String APPVersion="00.00.00.01";
     @Override
     public void onCreate() {
