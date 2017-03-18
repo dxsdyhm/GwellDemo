a99e0e4b75170b8705ae494dea12d3490adc3357
USER <duanxuesong12@126.com>
2017-03-18 14:11:45 +0800

    1.监控页修改代码 Butterknife

--- app/src/main/java/com/example/dansesshou/jcentertest/MonitoerActivity.java
+++ app/src/main/java/com/example/dansesshou/jcentertest/MonitoerActivity.java
@@ -26,27 +26,57 @@ import com.p2p.core.P2PHandler;
 import com.p2p.core.P2PValue;
 import com.p2p.core.P2PView;
 
-public class MonitoerActivity extends BaseMonitorActivity implements View.OnClickListener{
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MonitoerActivity extends BaseMonitorActivity{
     public static String P2P_ACCEPT = "com.yoosee.P2P_ACCEPT";
     public static String P2P_READY = "com.yoosee.P2P_READY";
     public static String P2P_REJECT = "com.yoosee.P2P_REJECT";
-    private Button btnCall,btnTalk,btnSD,btnHD,btnLD,btnMute,btnScreenshot;
-    private EditText et_callId;
-    private EditText et_callPwd ;
-    private String callID,CallPwd;
-    private TextView tv_content,tx_acount;
+    @BindView(R.id.rl_p2pview)
+    RelativeLayout rlP2pview;
+    @BindView(R.id.et_id)
+    EditText etId;
+    @BindView(R.id.et_pwd)
+    EditText etPwd;
+    @BindView(R.id.btn_call)
+    Button btnCall;
+    @BindView(R.id.btn_sd)
+    Button btnSd;
+    @BindView(R.id.btn_hd)
+    Button btnHd;
+    @BindView(R.id.btn_ld)
+    Button btnLd;
+    @BindView(R.id.tx_acount)
+    TextView txAcount;
+    @BindView(R.id.btn_screenshot)
+    Button btnScreenshot;
+    @BindView(R.id.btn_mute)
+    Button btnMute;
+    @BindView(R.id.btn_talk)
+    Button btnTalk;
+    @BindView(R.id.btn_no)
+    Button btnNo;
+    @BindView(R.id.tv_content)
+    TextView tvContent;
+    @BindView(R.id.layout_else)
+    LinearLayout layoutElse;
+    @BindView(R.id.activity_monitoer)
+    LinearLayout activityMonitoer;
+    private String callID, CallPwd;
     private String LoginID;
-    private boolean isMute=false;
+    private boolean isMute = false;
     OrientationEventListener mOrientationEventListener;
-    private boolean mIsLand=false;
-    private RelativeLayout rl_p2pview;
-    private int screenWidth,screenHeigh;
-    private LinearLayout llElse;
+    private boolean mIsLand = false;
+    private int screenWidth, screenHeigh;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_monitoer);
-        LoginID=getIntent().getStringExtra("LoginID");
+        ButterKnife.bind(this);
+        LoginID = getIntent().getStringExtra("LoginID");
         initUI();
         getScreenWithHeigh();
         regFilter();
@@ -62,33 +92,13 @@ public class MonitoerActivity extends BaseMonitorActivity implements View.OnClic
     }
 
     private void initUI() {
-        btnCall= (Button) findViewById(R.id.btn_call);
-        btnTalk= (Button) findViewById(R.id.btn_talk);
-        btnSD= (Button) findViewById(R.id.btn_sd);
-        btnHD= (Button) findViewById(R.id.btn_hd);
-        btnLD= (Button) findViewById(R.id.btn_ld);
-        btnMute= (Button) findViewById(R.id.btn_mute);
-        btnScreenshot= (Button) findViewById(R.id.btn_screenshot);
-
-        pView= (P2PView) findViewById(R.id.p2pview);
-        llElse= (LinearLayout) findViewById(R.id.layout_else);
-        rl_p2pview= (RelativeLayout) findViewById(R.id.rl_p2pview);
-        tv_content = (TextView) findViewById(R.id.tv_content);
-        et_callId = (EditText) findViewById(R.id.et_id);
-        et_callPwd = (EditText) findViewById(R.id.et_pwd);
-        tx_acount= (TextView) findViewById(R.id.tx_acount);
+        //pView已在父类声明，不要在子类重复
+        pView = (P2PView) findViewById(R.id.p2pview);
         initP2PView(7, P2PView.LAYOUTTYPE_TOGGEDER);//7是设备类型(技威定义的)
-        btnCall.setOnClickListener(this);
-
-        btnSD.setOnClickListener(this);
-        btnHD.setOnClickListener(this);
-        btnLD.setOnClickListener(this);
-        btnMute.setOnClickListener(this);
-        btnScreenshot.setOnClickListener(this);
         btnTalk.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
-                switch (event.getAction()){
+                switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
                         setMute(false);
                         return true;
@@ -100,8 +110,9 @@ public class MonitoerActivity extends BaseMonitorActivity implements View.OnClic
             }
         });
     }
+
     private void initData() {
-        tx_acount.setText(LoginID);
+        tvContent.setText(LoginID);
         //此处是一种并不常见的横竖屏监听,客户可自行修改实现
         mOrientationEventListener = new OrientationEventListener(this) {
 
@@ -130,10 +141,10 @@ public class MonitoerActivity extends BaseMonitorActivity implements View.OnClic
     @Override
     public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
-        Log.e("dxsTest","config:"+newConfig.orientation);
-        if(newConfig.orientation==Configuration.ORIENTATION_LANDSCAPE){
+        Log.e("dxsTest", "config:" + newConfig.orientation);
+        if (newConfig.orientation == Configuration.ORIENTATION_LANDSCAPE) {
             setHalfScreen(false);
-            llElse.setVisibility(View.GONE);
+            layoutElse.setVisibility(View.GONE);
             //以下代码是因为 方案商设备类型很多,视频比例也比较多
             //客户更具自己的视频比例调整画布大小
             //这里的实现比较绕,如果能弄清楚这部分原理,客户可自行修改此处代码
@@ -157,10 +168,10 @@ public class MonitoerActivity extends BaseMonitorActivity implements View.OnClic
             }
             LinearLayout.LayoutParams parames = new LinearLayout.LayoutParams(
                     LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
-            rl_p2pview.setLayoutParams(parames);//调整画布容器宽高(比例)
-        }else{
+            rlP2pview.setLayoutParams(parames);//调整画布容器宽高(比例)
+        } else {
             setHalfScreen(true);
-            llElse.setVisibility(View.VISIBLE);
+            layoutElse.setVisibility(View.VISIBLE);
             if (isFullScreen) {
                 isFullScreen = false;
                 pView.halfScreen();
@@ -171,50 +182,51 @@ public class MonitoerActivity extends BaseMonitorActivity implements View.OnClic
                     LinearLayout.LayoutParams.MATCH_PARENT,
                     LinearLayout.LayoutParams.WRAP_CONTENT);
             parames.height = Heigh;
-            rl_p2pview.setLayoutParams(parames);
+            rlP2pview.setLayoutParams(parames);
         }
     }
-
-    @Override
-    public void onClick(View v) {
-        callID = et_callId.getText().toString().trim();//设备号
-        CallPwd = et_callPwd.getText().toString().trim();
+    @OnClick(R.id.btn_call)
+    void CallOnClick(){
+        callID = etId.getText().toString().trim();//设备号
+        CallPwd = etPwd.getText().toString().trim();
         String pwd = P2PHandler.getInstance().EntryPassword(CallPwd);//经过转换后的设备密码
-        switch (v.getId()){
-            case R.id.btn_call:
-                P2PHandler.getInstance().call(LoginID, pwd, true, 1,callID, "", "", 2,callID);
-                break;
-            case R.id.btn_hd:
-                P2PHandler.getInstance().setVideoMode(P2PValue.VideoMode.VIDEO_MODE_HD);
-                break;
-            case R.id.btn_sd:
-                P2PHandler.getInstance().setVideoMode(P2PValue.VideoMode.VIDEO_MODE_SD);
-                break;
-            case R.id.btn_ld:
-                P2PHandler.getInstance().setVideoMode(P2PValue.VideoMode.VIDEO_MODE_LD);
-                break;
-            case R.id.btn_screenshot:
-                // 参数是一个标记,截图回调会原样返回这个标记
-                //注意SD卡权限
-                int d=P2PHandler.getInstance().setScreenShotpath("/sdcard/11/22/33","123.jpg");
-                Log.e("dxsTest","d:"+d);
-                captureScreen(-1);
-                break;
-            case R.id.btn_mute:
-                changeMuteState();
-                break;
-        }
+        P2PHandler.getInstance().call(LoginID, pwd, true, 1, callID, "", "", 2, callID);
     }
 
-    private void changeMuteState(){
-        isMute=!isMute;
-        AudioManager manager= (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-        if(manager!=null){
-            if(isMute){
-                manager.setStreamVolume(AudioManager.STREAM_MUSIC,0,0);
+    @OnClick(R.id.btn_sd)
+    void SDOnclick(){
+        P2PHandler.getInstance().setVideoMode(P2PValue.VideoMode.VIDEO_MODE_SD);
+    }
+    @OnClick(R.id.btn_ld)
+    void LDOnClick(){
+        P2PHandler.getInstance().setVideoMode(P2PValue.VideoMode.VIDEO_MODE_LD);
+    }
+    @OnClick(R.id.btn_hd)
+    void HDOnClick(){
+        P2PHandler.getInstance().setVideoMode(P2PValue.VideoMode.VIDEO_MODE_HD);
+    }
+    @OnClick(R.id.btn_screenshot)
+    void ScreenShotClock(){
+        // 参数是一个标记,截图回调会原样返回这个标记
+        //注意SD卡权限
+        int d = P2PHandler.getInstance().setScreenShotpath("/sdcard/11/22/33", "123.jpg");
+        Log.e("dxsTest", "d:" + d);
+        captureScreen(-1);
+    }
+    @OnClick(R.id.btn_mute)
+    void MuteOnClick(){
+        changeMuteState();
+    }
+
+    private void changeMuteState() {
+        isMute = !isMute;
+        AudioManager manager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+        if (manager != null) {
+            if (isMute) {
+                manager.setStreamVolume(AudioManager.STREAM_MUSIC, 0, 0);
                 btnMute.setText("");
-            }else{
-                manager.setStreamVolume(AudioManager.STREAM_MUSIC,manager.getStreamMaxVolume(AudioManager.STREAM_MUSIC),0);
+            } else {
+                manager.setStreamVolume(AudioManager.STREAM_MUSIC, manager.getStreamMaxVolume(AudioManager.STREAM_MUSIC), 0);
                 btnMute.setText(R.string.mute);
             }
         }
@@ -231,19 +243,19 @@ public class MonitoerActivity extends BaseMonitorActivity implements View.OnClic
     public BroadcastReceiver mReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
-            if(intent.getAction().equals(P2P_ACCEPT)){
+            if (intent.getAction().equals(P2P_ACCEPT)) {
                 int[] type = intent.getIntArrayExtra("type");
                 P2PView.type = type[0];
                 P2PView.scale = type[1];
-                tv_content.append("\n 监控数据接收");
-                Log.e("dxsTest","监控数据接收:"+callID);
+                tvContent.append("\n 监控数据接收");
+                Log.e("dxsTest", "监控数据接收:" + callID);
                 P2PHandler.getInstance().openAudioAndStartPlaying(1);//打开音频并准备播放，calllType与call时type一致
-            }else if(intent.getAction().equals(P2P_READY)){
-                tv_content.append("\n 监控准备,开始监控");
-                Log.e("dxsTest","监控准备,开始监控"+callID);
+            } else if (intent.getAction().equals(P2P_READY)) {
+                tvContent.append("\n 监控准备,开始监控");
+                Log.e("dxsTest", "监控准备,开始监控" + callID);
                 pView.sendStartBrod();
-            }else if(intent.getAction().equals(P2P_REJECT)){
-                tv_content.append("\n 监控挂断");
+            } else if (intent.getAction().equals(P2P_REJECT)) {
+                tvContent.append("\n 监控挂断");
             }
         }
     };
@@ -271,10 +283,10 @@ public class MonitoerActivity extends BaseMonitorActivity implements View.OnClic
 
     @Override
     protected void onCaptureScreenResult(boolean isSuccess, int prePoint) {
-        if(isSuccess){
-            Toast.makeText(this,R.string.screenshot_success,Toast.LENGTH_LONG).show();
-        }else{
-            Toast.makeText(this,R.string.screenshot_error,Toast.LENGTH_LONG).show();
+        if (isSuccess) {
+            Toast.makeText(this, R.string.screenshot_success, Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(this, R.string.screenshot_error, Toast.LENGTH_LONG).show();
         }
     }
 
