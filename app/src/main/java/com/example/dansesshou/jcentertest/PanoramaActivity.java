8a266c45edb29a6342be669c53bd4c60631eec2a
lianshangyangguang <1141179268@qq.com>
2018-04-23 18:05:23 +0800

    优化全景监控

--- app/src/main/java/com/example/dansesshou/jcentertest/PanoramaActivity.java
+++ app/src/main/java/com/example/dansesshou/jcentertest/PanoramaActivity.java
@@ -6,7 +6,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
